75a8092e4438ce775ec38e9e8a16bac50b6ee6cc
Paulin Antoine <antoine_paulin@yahoo.com>
2022-03-15 15:15:49 +0100

    Initialisation du projet

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raccourcisseur d'url</title>
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+    <link rel="icon" type="image/png" href="pictures/favico.png">
+</head>
+
+<body>
+    <section id="hello">
+        <div class="container">
+            <header>
+                <img src="pictures/logo.png" id="logo">
+            </header>
+            <h1>Une url longue ? Raccourissez-là</h1>
+            <h2>Largement meilleur et plus court que les autres !<h2>
+        </div>
+    </section>
+</body>
+
+</html>
