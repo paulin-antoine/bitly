cf87929f9fe821a8c1d2fae552e8f20e8a09a7ed
Paulin Antoine <antoine_paulin@yahoo.com>
2022-03-23 16:03:55 +0100

    fin du projet

--- index.php
+++ index.php
@@ -1,29 +1,144 @@
+<!-- L'envoie du formulaire -->
+<?php
+
+
+if (isset($_GET['q'])) {
+
+
+    $shortcut = htmlspecialchars($_GET['q']);
+
+
+    $bdd = new PDO('mysql:host=localhost;dbname=bitly;charset=utf8', 'root', '');
+    $req = $bdd->prepare('SELECT COUNT(*) AS x FROM links WHERE shortcut = ?');
+    $req->execute(array($shortcut));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 1) {
+            header('location: ../?error=true&message=Adresse url non connue');
+            exit();
+        }
+    }
+
+
+    $req = $bdd->prepare('SELECT * FROM links WHERE shortcut = ?');
+    $req->execute(array($shortcut));
+
+    while ($result = $req->fetch()) {
+
+        header('location: ' . $result['url']);
+        exit();
+    }
+}
+
+
+if (isset($_POST['url'])) {
+
+
+    $url = $_POST['url'];
+
+
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+
+        header('location: ../?error=true&message=Adresse url non valide');
+        exit();
+    }
+
+
+    $shortcut = crypt($url, rand());
+
+
+    $bdd = new PDO('mysql:host=localhost;dbname=bitly;charset=utf8', 'root', '');
+    $req = $bdd->prepare('SELECT COUNT(*) AS x FROM links WHERE url = ?');
+    $req->execute(array($url));
+
+    while ($result = $req->fetch()) {
+
+        if ($result['x'] != 0) {
+            header('location: ../?error=true&message=Adresse déjà raccourcie');
+            exit();
+        }
+    }
+
+
+    $req = $bdd->prepare('INSERT INTO links(url, shortcut) VALUES(?, ?)');
+    $req->execute(array($url, $shortcut));
+
+    header('location: ../?short=' . $shortcut);
+    exit();
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raccourcisseur d'url</title>
+    <meta charset="utf-8">
+    <title>Raccourcisseur d'url express</title>
     <link rel="stylesheet" type="text/css" href="default.css">
     <link rel="icon" type="image/png" href="pictures/favico.png">
 </head>
 
 <body>
+
     <section id="hello">
+
+
         <div class="container">
+
+
             <header>
-                <img src="pictures/logo.png" id="logo">
+                <img src="pictures/logo.png" alt="logo" id="logo">
             </header>
-            <h1>Une url longue ? Raccourissez-là.</h1>
-            <h2>Largement meilleur et plus court que les autres !<h2>
-                    <form method="post" action="../">
-                        <input type="url" name="url" placeholder="collez un lien à raccourcir">
-                        <input type="submit" value="Raccourcir">
-                    </form>
+
+
+            <h1>Une url longue ? Raccourcissez-là ?</h1>
+            <h2>Largement meilleur et plus court que les autres.</h2>
+
+
+            <form method="post" action="../">
+                <input type="url" name="url" placeholder="Collez un lien à raccourcir">
+                <input type="submit" value="Raccourcir">
+            </form>
+
+            <?php if (isset($_GET['error']) && isset($_GET['message'])) { ?>
+                <div class="center">
+                    <div id="result">
+                        <b><?php echo htmlspecialchars($_GET['message']); ?></b>
+                    </div>
+                </div>
+            <?php } else if (isset($_GET['short'])) { ?>
+                <div class="center">
+                    <div id="result">
+                        <b>URL RACCOURCIE : </b>
+                        http://localhost/?q=<?php echo htmlspecialchars($_GET['short']); ?>
+                    </div>
+                </div>
+            <?php } ?>
+
         </div>
+
     </section>
+
+
+    <section id="brands">
+
+
+        <div class="container">
+            <h3>Ces marques nous font confiance</h3>
+            <img src="pictures/1.png" alt="1" class="picture">
+            <img src="pictures/2.png" alt="2" class="picture">
+            <img src="pictures/3.png" alt="3" class="picture">
+            <img src="pictures/4.png" alt="4" class="picture">
+        </div>
+
+    </section>
+
+
+    <footer>
+        <img src="pictures/logo2.png" alt="logo" id="logo"><br>
+        2018 © Bitly<br>
+        <a href="#">Contact</a> - <a href="#">À propos</a>
+    </footer>
 </body>
 
 </html>
